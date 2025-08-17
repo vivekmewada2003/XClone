bfaf2953e5fd12dedf16c61d985d9c44a4f4cf07
Vivek-mewada <vmewada205@gmail.com>
2025-08-17 14:08:05 +0530

    Update all the dependencies in gemlock file

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @post = Post.new(user: @user, content: "This is a test post")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user should be present" do
+    @post.user = nil
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = "   "
+    assert_not @post.valid?
+  end
+
+  test "should broadcast after create" do
+    assert_broadcasts('all_posts', 1) do
+      @post.save
+    end
+  end
+
+  test "should broadcast after update" do
+    @post.save
+    assert_broadcasts('all_posts', 1) do
+      @post.update(content: "Updated content")
+    end
+  end
+
+  test "should broadcast after destroy" do
+    @post.save
+    assert_broadcasts('all_posts', 1) do
+      @post.destroy
+    end
+  end
 end
