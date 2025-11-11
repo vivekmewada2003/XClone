7e22f3e0775d029ef182790b4934ffef385a5398
Vivek Mewada <vmewada205@gmail.com>
2025-11-11 16:17:24 +0530

    Add render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
-bundle exec rails assests:precompile
-bundle exec rails assests:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
